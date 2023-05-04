c58743380740e0a8e96f2e7ef0b9282bc373b443
lhsribas <lhs.ribas@gmail.com>
2023-05-04 18:49:29 +0100

    adding test for kong exit transformer

--- src/main/java/com/movie/app/resource/MovieResource.java
+++ src/main/java/com/movie/app/resource/MovieResource.java
@@ -22,7 +22,7 @@ public class MovieResource {
     @GET
     @Path("/genre/{genre}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response gwtByGenres(@PathParam("genre") final String genre) {
+    public Response getByGenres(@PathParam("genre") final String genre) {
         return Response.ok()
                 .entity(new MovieService().getMoviesByGenres(genre))
                 .build();
@@ -31,9 +31,130 @@ public class MovieResource {
     @GET
     @Path("/year/{year}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response gwtByYear(@PathParam("year") final String year) {
+    public Response getByYear(@PathParam("year") final String year) {
         return Response.ok()
                 .entity(new MovieService().getMoviesByYear(year))
+                .build();
+    }
+
+    @GET
+    @Path("/400")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get400(){
+        return Response.status(400)
+                .entity("{\"message\": \"400\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/401")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get401(){
+        return Response.status(401)
+                .entity("{\"message\": \"401\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/403")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get403(){
+        return Response.status(403)
+                .entity("{\"message\": \"403\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/404")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get404(){
+        return Response.status(404)
+                .entity("{\"message\": \"404\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/405")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get405(){
+        return Response.status(405)
+                .entity("{\"message\": \"405\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/408")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get408(){
+        return Response.status(408)
+                .entity("{\"message\": \"408\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/409")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get409(){
+        return Response.status(409)
+                .entity("{\"message\": \"409\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/413")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get413(){
+        return Response.status(413)
+                .entity("{\"message\": \"413\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/415")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get415(){
+        return Response.status(415)
+                .entity("{\"message\": \"415\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/422")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get422(){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("{ \"status\": 422,");
+        sb.append("\"errorCode\": \"EI_VALIDATION_ERROR_422\",");
+        sb.append("\"message\":\"The request Error\",");
+        sb.append("\"devMessage\":\"Dev Messsage Error\",");
+        sb.append("\"fieldErrors\": [");
+        sb.append("{\"field\": \"type\",");
+        sb.append("\"code\": \"NotNull\",");
+        sb.append("\"rejectedValue\": null,");
+        sb.append("\"message\": \"type can not be null\"");
+        sb.append("}]}");
+
+        return Response.status(422)
+                .entity(sb.toString())
+                .build();
+    }
+
+    @GET
+    @Path("/500")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get500(){
+        return Response.status(500)
+                .entity("{\"message\": \"500\"}")
+                .build();
+    }
+
+    @GET
+    @Path("/503")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get503(){
+        return Response.status(503)
+                .entity("{\"message\": \"503\"}")
                 .build();
     }
 }
--- src/main/java/com/movie/app/service/MovieService.java
+++ src/main/java/com/movie/app/service/MovieService.java
@@ -5,6 +5,7 @@ import com.movie.domain.usecase.MovieSearchUserCase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MovieService {
 
@@ -22,14 +23,14 @@ public class MovieService {
         return this.useCase.getMovieRoot().getMovies()
                 .stream()
                 .filter(movies ->  contains(genre, movies.getGenres()))
-                .toList();
+                .collect(Collectors.toList());
     }
 
     public List<Movie> getMoviesByYear(final String year){
         return this.useCase.getMovieRoot().getMovies()
                 .stream()
                 .filter(movies -> movies.getYear().equals(year))
-                .toList();
+                .collect(Collectors.toList());
     }
 
     private boolean contains(final String str, final ArrayList<String> strArray) {
@@ -39,6 +40,7 @@ public class MovieService {
 
         return strArray.stream()
                 .map(String::toUpperCase)
-                .toList().contains(str.toUpperCase());
+                .collect(Collectors.toList())
+                .contains(str.toUpperCase());
     }
 }
